70b1c9fa5f893281e0e36f30d5a76d8d2451a9d3
rohfosho <Rohvar98@gmail.com>
2014-12-11 23:28:56 -0500

    moved content and server set up to app.js

--- app.js
+++ app.js
@@ -56,5 +56,10 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 
 module.exports = app;
