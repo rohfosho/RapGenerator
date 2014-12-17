e4d1194ee871f206b9d18ca48885fd50f5721a66
Paul Vorobyev <paul.vorobyev@gmail.com>
2014-12-17 10:29:53 -0500

    simple frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Genius Rap' });
+  res.render('index', { newRap: 'RapGoesHere' });
 });
 
 module.exports = router;
