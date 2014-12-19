57c0a62395c9590c94746ab261a58d4c5db1df86
rohfosho <Rohvar98@gmail.com>
2014-12-18 20:22:19 -0500

    lyrics scraped, andre 3000 and bino

---
+++ lyrics/scrape.py
@@ -0,0 +1,19 @@
+from urlparse import urljoin
+from bs4 import BeautifulSoup
+import requests
+
+
+BASE_URL = "http://genius.com"
+artist_url = "http://genius.com/artists/Childish-gambino/"
+
+response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+
+soup = BeautifulSoup(response.text, "lxml")
+for song_link in soup.select('ul.song_list > li > a'):
+    link = urljoin(BASE_URL, song_link['href'])
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text)
+    lyrics = soup.find('div', class_='lyrics').text.strip()
+    text_file = open(song_link.text.encode('utf-8').strip()+".txt","w")
+    text_file.write(lyrics.encode('utf-8').strip())
+    text_file.close()
