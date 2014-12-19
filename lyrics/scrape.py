d2e865f1c4a1c059e130f5868bca8c67667d698b
rohfosho <Rohvar98@gmail.com>
2014-12-18 21:03:42 -0500

    shit ton of more songs

--- lyrics/scrape.py
+++ lyrics/scrape.py
@@ -4,7 +4,7 @@ import requests
 
 
 BASE_URL = "http://genius.com"
-artist_url = "http://genius.com/artists/Kendrick-lamar/"
+artist_url = "http://genius.com/artists/Jay-Rock/"
 
 response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
 
@@ -14,6 +14,6 @@ for song_link in soup.select('ul.song_list > li > a'):
     response = requests.get(link)
     soup = BeautifulSoup(response.text)
     lyrics = soup.find('div', class_='lyrics').text.strip()
-    text_file = open(song_link.text.encode('utf-8').strip()+".txt","w")
+    text_file = open(song_link.text.encode('utf-8').strip()[0:25]+".txt","w")
     text_file.write(lyrics.encode('utf-8').strip())
     text_file.close()
