e1d29f89d5de2d5f55ada18be0c99ddfd7603c50
rohfosho <Rohvar98@gmail.com>
2014-12-20 22:39:05 -0500

    wtf am i doing

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var glob = require('glob');
 var MarkovChain = require('markovchain').MarkovChain;
+var fs = require('fs');
 
 var lyricFiles;
 
@@ -16,17 +17,34 @@ router.get('/', function(req, res){
 function mashEmUp(){
   var lyricz = "";
   var songsToMash = new Array(Math.floor(Math.random()*6+2));
+  var lengths = new Array(songsToMash.length);
   
   for(var i=0;i<songsToMash.length;i++){
-    songsToMash[i] = new MarkovChain({files: lyricFiles[Math.floor(Math.random()*lyricFiles.length)].substring(7)});
+    var el = Math.floor(Math.random()*lyricFiles.length);
+    songsToMash[i] = new MarkovChain({files: lyricFiles[el]});
+    lengths[i] = fs.readFileSync(lyricFiles[el]).toString().split('n').length-1;
+   // console.log(lengths[i]);
+   }
+
+  var index = 0;
+  var word = 'A';
+  for(var i=0; i<Math.floor(Math.random()*10+10); i++){
+    var size = Math.floor(Math.random()*lengths[index]);
+    songsToMash[index].start(word).end(size+1).process(
+      function (err,sentence) { 
+        if(err)
+          console.log('error:\t'+err);
+        else {
+          lyricz+=sentence; 
+          console.log(sentence);
+          var temp = sentence.split(' '); 
+        }
+      });
+    if(index < songsToMash.length-1)
+      index++;
+    else
+      index = 0;
   }
-
-  songsToMash.forEach(function(markovChain){
-    console.log(markovChain);
-  });
-
-
-
 
   return lyricz;
 
