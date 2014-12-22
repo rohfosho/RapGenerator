8d0c605b18207d230561278b245b1b87dcdf496d
rohfosho <Rohvar98@gmail.com>
2014-12-21 22:21:14 -0500

    lol shitty jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,23 @@
-console.log("Hello!")
+//console.log("hello world");
+function getNewRap(){
+  $.ajax({
+    url: '/api/',
+    type: 'GET',
+    success: function(data){
+       $('.generatedRapList').empty();
+       var temp = data.split('\n');
+       var lol = 0;
+       temp.forEach(function(element){
+         console.log(element);
+         if(lol > 0 && lol < temp.length-1)
+          $('.generatedRapList').append('<li>'+element+'</li>'); 
+        
+         lol++;
+       });
+       //$('.generatedRap').text(data);
+    },
+    error: function(error){
+      console.log(error.toString());
+    }
+  }); 
+}
